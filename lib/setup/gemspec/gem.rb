66b9e7df891106cf849b696ce42a1b6505b50ff1
Pavel Skrylev <majioa@altlinux.org>
2019-03-14 23:40:30 +0300

    cleanup
    - removed waste code in Gem gemspec.

--- lib/setup/gemspec/gem.rb
+++ lib/setup/gemspec/gem.rb
@@ -11,14 +11,5 @@ module Setup::Gemspec::Gem
       rescue Exception => e
          $stderr.puts "ERROR[#{e.class}]: #{e.message}"
       end
-
-      def fix_preloaded spec, gemspec, file
-         if spec.name == 'psych' && Psych.constants.include?(:VERSION)
-            Psych.send(:remove_const, :VERSION)
-            gemspec.parse(file)
-         else
-            spec
-         end
-      end
    end
 end
