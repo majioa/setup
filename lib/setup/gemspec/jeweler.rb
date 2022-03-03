f942b9c2186a75382caa8c405e849edb220fb2b4
Pavel Skrylev <majioa@altlinux.org>
2022-03-03 15:16:50 +0300

    fix: jeweler spec parser
    ! for jeweler spec parser to properly load and store specs

--- lib/setup/gemspec/jeweler.rb
+++ lib/setup/gemspec/jeweler.rb
@@ -14,16 +14,22 @@ module Setup::Gemspec::Jeweler
             module_name = "M" + Random.srand.to_s
             mod_code = <<-END
                module #{module_name}
+                  def self.specs
+                     @objects
+                  end
+
                   extend(Rake::DSL)
                   # NOTE this forces not to share namespace but avoid exception when calling
                   # main space methods, see Rakefile of racc gem
                   # also named module is required instead of anonymous one to allow root level defined methods access
                   load('#{propfile}')
+
+                  @objects = ObjectSpace.each_object(Jeweler::Tasks).map { |h| h.spec }.compact
                end
 
-               ObjectSpace.each_object(Jeweler::Tasks).map { |h| h.spec }.compact
+               #{module_name}
             END
-            specs = module_eval(mod_code)
+            specs = module_eval(mod_code).specs
          rescue Exception => e
             warn(e.message)
          else
